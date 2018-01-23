d46859344dff13ed552bef5edbd419a53eb27f6c
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-23 15:52:04 +1300

    Added other simple requests

--- simple-http-client/src/app/simple-http/simple-http.component.ts
+++ simple-http-client/src/app/simple-http/simple-http.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpRequest, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'simple-http',
@@ -20,6 +20,44 @@ export class SimpleHttpComponent implements OnInit {
     .subscribe(data => {
       this.data = data;
       this.loading = false;
+    });
+  }
+
+  makePost(): void{
+    this.loading = true;
+    this.http.post('https://jsonplaceholder.typicode.com/posts',
+      JSON.stringify({
+        body:'bar',
+        title:'foo',
+        userId:1
+      })
+    )
+    .subscribe(data => {
+      this.data = data;
+      this.loading = false;
+    });
+  }
+
+  makeDelete(): void{
+    this.loading = true;
+    this.http.delete('https://jsonplaceholder.typicode.com/posts/1')
+    .subscribe(data => {
+      this.data = data;
+      this.loading = false;
+    });
+  }
+
+  makeHeaders():void{
+    const headers: HttpHeaders = new HttpHeaders({
+      'X-API-TOKEN': 'ng-book'
+    });
+
+    const req = new HttpRequest(
+      'GET', 'https://jsonplaceholder.typicode.com/posts/1',{headers:headers}
+    );
+
+    this.http.request(req).subscribe(data => {
+      this.data = data['body'];
     });
   }
 
