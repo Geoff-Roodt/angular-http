e7dc3899af1c6721ed4cad8bce32b9ecac8027d4
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-23 14:50:40 +1300

    Added update results method and html

--- youtube-searcher/src/app/you-tube-search/you-tube-search.component.ts
+++ youtube-searcher/src/app/you-tube-search/you-tube-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResult } from './search-result.model';
 
 @Component({
   selector: 'you-tube-search',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./you-tube-search.component.css']
 })
 export class YouTubeSearchComponent implements OnInit {
+  results: SearchResult[];
+  loading: boolean;
 
   constructor() { }
+  
+  updateResults(results: SearchResult[]): void {
+    this.results = results;
+    // console.log("results:", this.results); // uncomment to take a look
+  }
 
   ngOnInit() {
   }
