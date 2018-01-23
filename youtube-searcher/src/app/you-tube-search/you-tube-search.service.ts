939614c86d6cea1ac5320304d78b134acd61a2db
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-23 14:53:38 +1300

    Replaced with my own key

--- youtube-searcher/src/app/you-tube-search/you-tube-search.service.ts
+++ youtube-searcher/src/app/you-tube-search/you-tube-search.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { SearchResult } from './search-result.model';
 
-export const YOUTUBE_API_KEY = 'AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk';
+export const YOUTUBE_API_KEY = 'AIzaSyAaXpmZz7L541X2AYTP6jK24h5EcsH-nfk';
 export const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 @Injectable()
@@ -30,7 +30,7 @@ export class YouTubeSearchService {
           thumbnailUrl: item.snippet.thumbnails.high.url
         });
       });
-    });    
+    });
   }
 
 }
