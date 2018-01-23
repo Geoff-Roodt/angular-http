3ff552233f9545943ace376511b5987b3ab23135
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-23 15:30:04 +1300

    removed un-necessary components

--- simple-http-client/src/app/app.module.ts
+++ simple-http-client/src/app/app.module.ts
@@ -5,28 +5,19 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
-import { SearchResultComponent } from './youtube-search/search-result.component';
-import { YoutubeSearchComponent } from './youtube-search/youtube-search.component';
-import {SearchBoxComponent} from './youtube-search/search-box.component';
-import {YouTubeSearchInjectables} from './youtube-search/youtube-search.injectables';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent,
-    SearchResultComponent,
-    YoutubeSearchComponent,
-    SearchBoxComponent
+    SimpleHttpComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule
   ],
-  providers: [
-    YouTubeSearchInjectables
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
